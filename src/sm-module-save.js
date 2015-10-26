d3e72d7b6d479722719e9e61fd21fa2e8efd8a08
Bede <bede.overend@gmail.com>
2015-10-27 08:24:51 +1100

    add save function

--- src/sm-module-save.js
+++ src/sm-module-save.js
@@ -15,6 +15,33 @@ class SmModuleSave {
     ];
   }
 
+  save() {
+    let leftToSave = 0,
+        haveTriedAll = false;
+
+    simpla.elements.forEach(element => {
+      let willSave,
+          saved;
+
+      saved = () => {
+        leftToSave--;
+        element.removeEventListener('saved', saved);
+
+        if (leftToSave === 0 && haveTriedAll) {
+          this.fire('saved');
+        }
+      };
+
+      willSave = element.save();
+
+      if (willSave) {
+        leftToSave++;
+        element.addEventListener('saved', saved);
+      }
+    });
+
+    haveTriedAll = true;
+  }
 }
 
 Polymer(SmModuleSave);
