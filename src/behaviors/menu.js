fe46200baf64fe149cfc5cd14d1fb76a3b81bad6
Sean King <sean@seanking.org>
2015-12-29 13:28:07 +1100

    build menu from object array and refactor behavior

--- src/behaviors/menu.js
+++ src/behaviors/menu.js
@@ -1,59 +1,104 @@
-const easings = simpla.constants.easings;
-
-export default {
-  observers: [
-    '_toggleMenu(menuActive)'
-  ],
-
-  _toggleMenu(menuActive) {
-    if (menuActive){
-      this._openMenu();
-    } else {
-      this._closeMenu();
-    }
-  },
-
-  get _menuAnimation() {
-    let menu = this.$.menu;
-
-    return {
-      target: menu,
-      frames: [
-        { transform: 'translateY(-100%)' },
-        { transform: 'translateY(0)' }
-      ],
-      opts: {
-        open: {
-          easing: easings.easeOutBack,
-          fill: 'both',
-          duration: 200,
-          delay: 15
-        },
-        close: {
+const easings = simpla.constants.easings,
+      ANIMATION = {
+        frames: [
+          { transform: 'translateY(-100%)' },
+          { transform: 'translateY(0)' }
+        ],
+        opts: {
           easing: easings.easeOutCubic,
           fill: 'both',
           duration: 180
         }
-      }
+      };
+
+export default {
+
+  properties: {
+
+    // Array of menu item objects
+    // { id: String, text: String, icon: String, onTap: function }
+    menu: Array,
+
+    // Keep track of whether menu is open or closed
+    _menuActive: {
+      type: Boolean,
+      observer: '_menuActiveChanged'
+    },
+
+    // Icon on the menu toggle button
+    // (computed based on state)
+    _menuIcon: {
+      type: String,
+      computed: '_computeMenuIcon(_menuActive)',
+      value: 'simpla:arrow-down'
     }
+
   },
 
+  /**
+   * Convinience method to toggle the _menuActive property
+   * @return undefiend
+   */
+  toggleMenu() {
+    this._menuActive = !this._menuActive;
+  },
+
+  /**
+   * Open and close the menu on _menuActive change
+   * @param  {Boolean} active state of the _menuActive property
+   * @return undefined
+   */
+  _menuActiveChanged(active) {
+    active ? this._openMenu() : this._closeMenu()
+  },
+
+  /**
+   * Animate the menu in
+   * @return undefined
+   */
   _openMenu(){
-    let { target, frames, opts } = this._menuAnimation;
+    let menu = this.$.menu;
 
-    this.toggleAttribute('visible', true, target);
-    target.animate(frames, opts.open);
+    this.toggleAttribute('visible', true, menu);
+    menu.animate(ANIMATION.frames, ANIMATION.opts);
   },
 
+  /**
+   * Animate the menu out
+   * @return undefined
+   */
   _closeMenu() {
-    let { target, frames, opts } = this._menuAnimation,
+    let menu = this.$.menu,
         animation;
 
-    animation = target.animate(frames.reverse(), opts.close);
+    animation = menu.animate(ANIMATION.frames.reverse(), ANIMATION.opts);
     animation.onfinish = () => {
-      this.toggleAttribute('visible', false, target);
+      this.toggleAttribute('visible', false, menu);
     }
+  },
 
-  }
+  /**
+   * Compute menu icon based on menu state
+   * @param  {Boolean} _menuActive whether menu acitve or not (property)
+   * @return undefined
+   */
+  _computeMenuIcon(_menuActive) {
+    let icon;
+
+    _menuActive ? icon = 'simpla:arrow-up' : icon = 'simpla:arrow-down';
+    return icon;
+  },
+
+  /*********************
+   * Menu item actions *
+   *********************/
+
+  /**
+   * Trigger logout
+   * @return undefined
+   */
+  _logout() {
+    this.$.auth.logout();
+  },
 
 };
